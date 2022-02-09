cd97d4efb5f01da5c3c49294a982ebbc613b54d5
theosagras <42276726+theosagras@users.noreply.github.com>
2022-02-09 22:37:25 +0200

    Update uomTrack.js

--- uomTrack.js
+++ uomTrack.js
@@ -867,15 +867,6 @@ function removeKey(evt) {
 
 
 
-
-// Register service worker to control making site work offline
-
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-        .register('/pwa-examples/a2hs/sw.js')
-        .then(() => { console.log('Service Worker Registered'); });
-}
-
 // Code to handle install prompt on desktop
 
 let deferredPrompt;
